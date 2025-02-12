960d9e839c65871c4c11287b68306d1e78a5021e
David Yanez <dyanez@oshyn.com>
2025-02-12 13:04:00 -0500

    single quotes

--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -1,5 +1,5 @@
 export default function decorate(block) {
   const [quoteText, quoteAuthor] = block.children;
-  quoteText.className = "quoteText";
-  quoteAuthor.className = "quoteAuthor";
+  quoteText.className = 'quoteText';
+  quoteAuthor.className = 'quoteAuthor';
 }
