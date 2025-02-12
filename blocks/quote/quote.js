e10ca7ba672fc0bb3a2c94cb9b2c9839be96d77e
David Yanez <dyanez@oshyn.com>
2025-02-12 13:06:39 -0500

    quote styles

--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -1,5 +1,5 @@
 export default function decorate(block) {
   const [text, author] = block.children;
-  text.className = 'quoteText';
-  author.className = 'quoteAuthor';
+  text.className = 'text';
+  author.className = 'author';
 }
