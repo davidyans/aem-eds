266d9a9009abeeca132c2c026c4e666c8769e194
David Yanez <dyanez@oshyn.com>
2025-02-14 15:37:04 -0500

    css class

--- blocks/weather/weather.js
+++ blocks/weather/weather.js
@@ -1,4 +1,14 @@
 export default function decorate(block) {
   const [title] = block.children;
   title.className = 'weather-title';
+
+  var weather_title = block.querySelector('.weather-title');
+
+  if (title) {
+    const paragraph = title.querySelector('p');
+
+    if (paragraph) {
+      paragraph.classList.add('location');
+    }
+  }
 }
