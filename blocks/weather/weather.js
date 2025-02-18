57db15d7a3d6c0be75e47440f70e352f4758c652
David Yanez <dyanez@oshyn.com>
2025-02-18 10:59:39 -0500

    test

--- blocks/weather/weather.js
+++ blocks/weather/weather.js
@@ -1,17 +1,8 @@
 export default function decorate(block) {
-  const weatherTitle = block.querySelector('.weather-title');
 
   const [title] = block.children;
   title.className = 'weather-comp-container';
 
   const [subcontainer] = title.children;
   subcontainer.className = 'weather-comp-subcontainer';
-
-  if (weatherTitle) {
-    const paragraph = weatherTitle.querySelector('p');
-
-    if (paragraph) {
-      paragraph.classList.add('location');
-    }
-  }
 }
